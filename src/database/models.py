1693ea5c91bc8b40b6edc553ddb47a76cd29a6d4
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2024-01-09 21:47:34 +0200

    Create models.py

---
+++ src/database/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Date, DateTime, func
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, nullable=False)
+    second_name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone = Column(String, unique=True, index=True, nullable=False)
+    birth_date = Column(Date, nullable=False)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
